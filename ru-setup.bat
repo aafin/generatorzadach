ae55184b081ae4becede6cf1ebdd9ad14498e565
Anton Finogenov <aafin@ya.ru>
2009-11-07 09:28:53 +0500

    ??????? ??? ???????

--- ru-setup.bat
+++ ru-setup.bat
@@ -3,17 +3,13 @@ chcp 866
 
 
 
-:: Кол-во человек в группе по умолчанию
-:: Основная масса заданий.
-
-
 
 call .\tools.bat mkalldir ru 10
 
 echo chcp 866 > ru\mkcard.bat
 echo ..\tools.bat mkcard ru Билеты >> ru\mkcard.bat
 
-copy ru-smpl.txt ru\Образец.ini
+copy ru-smpl.txt ru\Obrazec.ini
 
 :end
 
